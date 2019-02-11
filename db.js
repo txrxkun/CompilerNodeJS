5c93f9098694c428270062b4a9ca78f12d8e46c0
prasetyo21 <prasetyobu21@gmail.com>
2019-02-11 14:50:44 +0700

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ class db {
         var con = mysql.createPool({
             connectionLimit: 100,
             host: '127.0.0.1:51992',
-            user: 'root',
-            password: 'Bakrie2018@@',
+            user: 'prasetyo',
+            password: 'hmtif18',
             database: 'compiler',
             port: '3306',
             multipleStatement: true
