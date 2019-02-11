510a576910131db763598c25f87323e76ad2e6ab
Mohammad Dimas <45448847+txrxkun@users.noreply.github.com>
2019-02-11 18:13:16 +0700

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ class db {
     connection(mysql) {
         var con = mysql.createPool({
             connectionLimit: 100,
-            host: '127.0.0.1',
-            user: 'azure',
-            password: '6',
+            host: 'compilerhmtif.mysql.database.azure.com',
+            user: 'hmtif_prasetyo@compilerhmtif',
+            password: 'Bakrie2018@@',
             database: 'compiler',
-            port: '51992',
+            port: '3306',
             multipleStatement: true
         });
         return con;
