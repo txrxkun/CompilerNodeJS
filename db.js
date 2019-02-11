15facf39245de80e430509da2d5eb6a786f712c3
Mohammad Dimas <45448847+txrxkun@users.noreply.github.com>
2019-02-11 18:02:54 +0700

    Update db.js
    edit akun baru

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ class db {
     connection(mysql) {
         var con = mysql.createPool({
             connectionLimit: 100,
-            host: '127.0.0.1:51992',
-            user: 'prasetyo',
-            password: 'hmtif18',
+            host: '127.0.0.1',
+            user: 'azure',
+            password: '6',
             database: 'compiler',
-            port: '3306',
+            port: '51992',
             multipleStatement: true
         });
         return con;
