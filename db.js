e0d0f26b7f5cdb14776eebcc1aedce3165d74d44
prasetyo21 <prasetyobu21@gmail.com>
2019-02-11 14:43:58 +0700

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ class db {
             connectionLimit: 100,
             host: '127.0.0.1:51992',
             user: 'root',
-            password: '',
+            password: 'Bakrie2018@@',
             database: 'compiler',
             port: '3306',
             multipleStatement: true
